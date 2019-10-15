db74c8d7dd2f625602842e338770fb937308710e
Karan Kumar <kumarkay.ca@gmail.com>
2019-10-15 18:12:18 -0400

    Initial Commit

---
+++ Swift_Inheritance/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_Inheritance
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
