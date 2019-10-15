1a5254af88855addb0fdafcb9028d175784f0a3d
Karan Kumar <kumarkay.ca@gmail.com>
2019-10-15 18:26:05 -0400

    Basic details of Inheritance  in ClassA File

---
+++ Swift_Inheritance/ClassA.swift
@@ -0,0 +1,30 @@
+//
+//  ClassA.swift
+//  Swift_Inheritance
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+//syntax :
+
+//Class B : A,IDisplay....          A is class here and IDisplay is a Interface
+
+//We can add n numbers of interfaces while inheritance thats why i put ... after IDisplay
+//But can only use 1 class to inheritance
+
+
+// Declaring The Interface(there is No Interface in Swift ... Swift use Protocols instead of Interfaces)
+// Function of both is same
+// Protocol IDisplay        Protocol is keyword to declare a Protocol(Interface)
+//{
+//    func display()
+//}
+
+
+
+
+
+
